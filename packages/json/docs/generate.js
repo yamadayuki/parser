1c7684ec34ef2023048ad271b9e7617bc15fef68
Yuki Yamada <yamadayuuki0818@gmail.com>
2019-06-26 22:53:44 +0900

    Genarate diagram

---
+++ packages/json/docs/generate.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const fs = require("fs");
+const { createSyntaxDiagramsCode } = require("chevrotain");
+const { JSONParser } = require(path.resolve(`${__dirname}/../lib/parser`));
+
+const parser = new JSONParser();
+const serializedGrammar = parser.getSerializedGastProductions();
+const htmlText = createSyntaxDiagramsCode(serializedGrammar);
+const outPath = path.resolve(__dirname, "./");
+fs.writeFileSync(`${outPath}/generated_diagrams.html`, htmlText);
