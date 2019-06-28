4f98c8bfabd997f6c0ebae6c610757a36d91389b
Yuki Yamada <yamadayuuki0818@gmail.com>
2019-06-28 14:59:03 +0900

    Implement lexer and parser

--- packages/toml/src/lexer.ts
+++ packages/toml/src/lexer.ts
@@ -11,9 +11,52 @@ function MAKE_PATTERN(pattern: string, flags?: string) {
   return build(pattern, fragments, flags);
 }
 
+REGISTER("Whitespace", "[ \\t]");
+REGISTER("Newline", "\\n|\\r\\n");
+REGISTER("CommentSymbol", "#");
+REGISTER("NonAscii", "[\\u{0080}-\\u{D7FF}|\\u{E000}-\\u{FFFF}]");
+REGISTER("NonEol", "[\\u{0009}|\\u{0020}-\\u{007F}|\\u{0080}-\\u{D7FF}|\\u{E000}-\\u{FFFF}]");
+REGISTER("Comment", "{{CommentSymbol}}{{NonEol}}*");
 REGISTER("Alphabet", "[A-Z|a-z]");
 REGISTER("DIGIT", "[0-9]");
 REGISTER("HEXDIGIT", "[0-9a-fA-F]");
+REGISTER("OCTALDIGIT", "[0-7]");
+REGISTER("BINARYDIGIT", "[0-1]");
+REGISTER(
+  "BasicUnescaped",
+  "[\\u{0020}-\\u{0021}|\\u{0023}-\\u{005B}|\\u{005D}-\\u{007E}|\\u{0080}-\\u{D7FF}|\\u{E000}-\\u{FFFF}]"
+);
+REGISTER("Escape", "\\\\"); // \
+REGISTER("EscapeSequenceChars", '["\\\\bfnrt|\\u{0000}-\\u{FFFF}]');
+REGISTER("Escaped", "{{Escape}}{{EscapeSequenceChars}}");
+REGISTER(
+  "BasicChar",
+  '[\\u0020-\\u0021]|[\\u0023-\\u005b]|[\\u005d-\\u007e]|[\\u0080-\\ud7ff]|[\\ue000-\\uffff]|\\\\["\\\\bfnrt|\\u0000-\\uffff]'
+);
+REGISTER("MlBasicUnescaped", "[\\u0020-\\u005B|\\u005D-\\u007E]|{{NonAscii}}");
+REGISTER("DigitWithUnderscore", "{{DIGIT}}|_{{DIGIT}}");
+REGISTER("UnsignedDecimalInteger", "{{DIGIT}}|[1-9]{{DigitWithUnderscore}}+");
+REGISTER("HexWithUnderscore", "{{HEXDIGIT}}|_{{HEXDIGIT}}");
+REGISTER("OctalWithUnderscore", "{{OCTALDIGIT}}|_{{OCTALDIGIT}}");
+REGISTER("BinaryWithUnderscore", "{{BINARYDIGIT}}|_{{BINARYDIGIT}}");
+REGISTER("DecimalInteger", "[+-]?{{UnsignedDecimalInteger}}");
+REGISTER("ZeroPrefixableInteger", "{{DIGIT}}{{DigitWithUnderscore}}");
+REGISTER("FractionPart", "\\.{{ZeroPrefixableInteger}}");
+REGISTER("Exponential", "e{{DecimalInteger}}");
+REGISTER("Year", "{{DIGIT}}{4}");
+REGISTER("Month", "{{DIGIT}}{2}");
+REGISTER("Day", "{{DIGIT}}{2}");
+REGISTER("TimeDelimiter", "[tT ]");
+REGISTER("Hour", "{{DIGIT}}{2}");
+REGISTER("Minute", "{{DIGIT}}{2}");
+REGISTER("Second", "{{DIGIT}}{2}");
+REGISTER("SecondFraction", "\\.{{DIGIT}}+");
+REGISTER("NumberOffset", "[+-]{{Hour}}:{{Minute}}");
+REGISTER("Offset", "Z|{{NumberOffset}}");
+REGISTER("PartialTime", "{{Hour}}:{{Minute}}:{{Second}}{{SecondFraction}}?");
+REGISTER("FullDate", "{{Year}}-{{Month}}-{{Day}}");
+REGISTER("FullTime", "{{PartialTime}}{{Offset}}");
+REGISTER("WhitespaceCommentNewline", "({{Whitespace}}|{{Comment}}{{Newline}})+");
 
 /**
  * Whitespace
@@ -24,7 +67,7 @@ REGISTER("HEXDIGIT", "[0-9a-fA-F]");
  */
 const Whitespace = createToken({
   name: "Whitespace",
-  pattern: /[ \t]/, // Space / Horizontal tab
+  pattern: MAKE_PATTERN("{{Whitespace}}"),
   group: Lexer.SKIPPED,
 });
 
@@ -36,7 +79,7 @@ const Whitespace = createToken({
  */
 export const Newline = createToken({
   name: "Newline",
-  pattern: /\n|\r\n/, // LF / CRLF
+  pattern: MAKE_PATTERN("{{Newline}}"),
 });
 
 /**
@@ -49,13 +92,9 @@ export const Newline = createToken({
  * comment = comment-start-symbol *non-eol
  */
 
-REGISTER("CommentSymbol", "#");
-REGISTER("NonAscii", "[\\u{0080}-\\u{D7FF}|\\u{E000}-\\u{FFFF}]");
-REGISTER("NonEol", "[\\u{0009}|\\u{0020}-\\u{007F}|\\u{0080}-\\u{D7FF}|\\u{E000}-\\u{FFFF}]");
-
 const Comment = createToken({
   name: "Comment",
-  pattern: MAKE_PATTERN("{{CommentSymbol}}{{NonEol}}*"),
+  pattern: MAKE_PATTERN("{{Comment}}"),
 });
 
 /**
@@ -87,18 +126,6 @@ const Comment = createToken({
  * escape-seq-char =/ %x55 8HEXDIG ; UXXXXXXXX            U+XXXXXXXX
  */
 
-REGISTER(
-  "BasicUnescaped",
-  "[\\u{0020}-\\u{0021}|\\u{0023}-\\u{005B}|\\u{005D}-\\u{007E}|\\u{0080}-\\u{D7FF}|\\u{E000}-\\u{FFFF}]"
-);
-REGISTER("Escape", "\\\\"); // \
-REGISTER("EscapeSequenceChars", '["\\\\bfnrt|\\u{0000}-\\u{FFFF}]');
-REGISTER("Escaped", "{{Escape}}{{EscapeSequenceChars}}");
-REGISTER(
-  "BasicChar",
-  '[\\u0020-\\u0021]|[\\u0023-\\u005b]|[\\u005d-\\u007e]|[\\u0080-\\ud7ff]|[\\ue000-\\uffff]|\\\\["\\\\bfnrt|\\u0000-\\uffff]'
-);
-
 export const Dq = createToken({
   name: "Dq",
   pattern: '"',
@@ -120,8 +147,6 @@ export const BasicChar = createToken({
  * ml-basic-char = ml-basic-unescaped / escaped
  * ml-basic-unescaped = %x20-5B / %x5D-7E / non-ascii
  */
-
-REGISTER("MlBasicUnescaped", "[\\u0020-\\u005B|\\u005D-\\u007E]|{{NonAscii}}");
 
 export const ThreeDq = createToken({
   name: "ThreeDq",
@@ -174,7 +199,303 @@ export const MultilineLiteralChar = createToken({
   pattern: MAKE_PATTERN("[\\u0009|\\u0020-\\u007E]|{{NonAscii}}"),
 });
 
+/**
+ * Integer
+ *
+ * integer = dec-int / hex-int / oct-int / bin-int
+ *
+ * minus = %x2D                       ; -
+ * plus = %x2B                        ; +
+ * underscore = %x5F                  ; _
+ * digit1-9 = %x31-39                 ; 1-9
+ * digit0-7 = %x30-37                 ; 0-7
+ * digit0-1 = %x30-31                 ; 0-1
+ *
+ * hex-prefix = %x30.78               ; 0x
+ * oct-prefix = %x30.6f               ; 0o
+ * bin-prefix = %x30.62               ; 0b
+ *
+ * dec-int = [ minus / plus ] unsigned-dec-int
+ * unsigned-dec-int = DIGIT / digit1-9 1*( DIGIT / underscore DIGIT )
+ *
+ * hex-int = hex-prefix HEXDIG *( HEXDIG / underscore HEXDIG )
+ * oct-int = oct-prefix digit0-7 *( digit0-7 / underscore digit0-7 )
+ * bin-int = bin-prefix digit0-1 *( digit0-1 / underscore digit0-1 )
+ */
+
+export const DecimalInteger = createToken({
+  name: "DecimalInteger",
+  pattern: MAKE_PATTERN("{{DecimalInteger}}"),
+});
+
+export const HexInteger = createToken({
+  name: "HexInteger",
+  pattern: MAKE_PATTERN("\\b0x{{HEXDIGIT}}{{HexWithUnderscore}}*\\b"),
+});
+
+export const OctInteger = createToken({
+  name: "OctInteger",
+  pattern: MAKE_PATTERN("\\b0o{{OCTALDIGIT}}{{OctalWithUnderscore}}*\\b"),
+});
+
+export const BinInteger = createToken({
+  name: "BinInteger",
+  pattern: MAKE_PATTERN("\\b0b{{BINARYDIGIT}}{{BinaryWithUnderscore}}*\\b"),
+});
+
+/**
+ * Float
+ *
+ * float = float-int-part ( exp / frac [ exp ] )
+ * float =/ special-float
+ *
+ * float-int-part = dec-int
+ * frac = decimal-point zero-prefixable-int
+ * decimal-point = %x2E               ; .
+ * zero-prefixable-int = DIGIT *( DIGIT / underscore DIGIT )
+ *
+ * exp = "e" float-int-part
+ *
+ * special-float = [ minus / plus ] ( inf / nan )
+ * inf = %x69.6e.66  ; inf
+ * nan = %x6e.61.6e  ; nan
+ */
+
+export const Float = createToken({
+  name: "Float",
+  pattern: MAKE_PATTERN("\\b{{DecimalInteger}}({{Exponential}}|{{FractionPart}}{{Exponential}})?\\b"),
+});
+
+export const SpecialFloat = createToken({
+  name: "SpecialFloat",
+  pattern: MAKE_PATTERN("[+-]?(inf|nan)"),
+});
+
+/**
+ * Boolean
+ *
+ * boolean = true / false
+ *
+ * true    = %x74.72.75.65     ; true
+ * false   = %x66.61.6C.73.65  ; false
+ */
+
+export const True = createToken({
+  name: "True",
+  pattern: "true",
+});
+
+export const False = createToken({
+  name: "False",
+  pattern: "false",
+});
+
+/**
+ * Date and Time (as defined in RFC 3339)
+ *
+ * date-time      = offset-date-time / local-date-time / local-date / local-time
+ *
+ * date-fullyear  = 4DIGIT
+ * date-month     = 2DIGIT  ; 01-12
+ * date-mday      = 2DIGIT  ; 01-28, 01-29, 01-30, 01-31 based on month/year
+ * time-delim     = "T" / %x20 ; T, t, or space
+ * time-hour      = 2DIGIT  ; 00-23
+ * time-minute    = 2DIGIT  ; 00-59
+ * time-second    = 2DIGIT  ; 00-58, 00-59, 00-60 based on leap second rules
+ * time-secfrac   = "." 1*DIGIT
+ * time-numoffset = ( "+" / "-" ) time-hour ":" time-minute
+ * time-offset    = "Z" / time-numoffset
+ *
+ * partial-time   = time-hour ":" time-minute ":" time-second [ time-secfrac ]
+ * full-date      = date-fullyear "-" date-month "-" date-mday
+ * full-time      = partial-time time-offset
+ *
+ * Offset Date-Time
+ *
+ * offset-date-time = full-date time-delim full-time
+ *
+ * Local Date-Time
+ *
+ * local-date-time = full-date time-delim partial-time
+ *
+ * Local Date
+ *
+ * local-date = full-date
+ *
+ * Local Time
+ *
+ * local-time = partial-time
+ */
+
+export const OffsetDateTime = createToken({
+  name: "OffsetDateTime",
+  pattern: MAKE_PATTERN("{{FullDate}}{{TimeDelimiter}}{{FullTime}}"),
+});
+
+export const LocalDateTime = createToken({
+  name: "LocalDateTime",
+  pattern: MAKE_PATTERN("{{FullDate}}{{TimeDelimiter}}{{PartialTime}}"),
+});
+
+export const LocalDate = createToken({
+  name: "LocalDate",
+  pattern: MAKE_PATTERN("{{FullDate}}"),
+});
+
+export const LocalTime = createToken({
+  name: "LocalTime",
+  pattern: MAKE_PATTERN("{{PartialTime}}"),
+});
+
+/**
+ * Array
+ *
+ * array = array-open [ array-values ] ws-comment-newline array-close
+ *
+ * array-open =  %x5B ; [
+ * array-close = %x5D ; ]
+ *
+ * array-values =  ws-comment-newline val ws array-sep array-values
+ * array-values =/ ws-comment-newline val ws [ array-sep ]
+ *
+ * array-sep = %x2C  ; , Comma
+ *
+ * ws-comment-newline = *( wschar / [ comment ] newline )
+ */
+
+export const LSquare = createToken({
+  name: "LSquare",
+  pattern: "[",
+});
+
+export const RSquare = createToken({
+  name: "RSquare",
+  pattern: "]",
+});
+
+export const Comma = createToken({
+  name: "Comma",
+  pattern: ",",
+});
+
+export const WhitespaceCommentNewline = createToken({
+  name: "WhitespaceCommentNewline",
+  pattern: MAKE_PATTERN("{{WhitespaceCommentNewline}}"),
+  group: Lexer.SKIPPED,
+});
+
+/**
+ * Key-Value pairs
+ *
+ * keyval = key keyval-sep val
+ *
+ * key = simple-key / dotted-key
+ * simple-key = quoted-key / unquoted-key
+ *
+ * unquoted-key = 1*( ALPHA / DIGIT / %x2D / %x5F ) ; A-Z / a-z / 0-9 / - / _
+ * quoted-key = basic-string / literal-string
+ * dotted-key = simple-key 1*( dot-sep simple-key )
+ *
+ * dot-sep   = ws %x2E ws  ; . Period
+ * keyval-sep = ws %x3D ws ; =
+ */
+
+export const UnquotedKey = createToken({
+  name: "UnquotedKey",
+  pattern: MAKE_PATTERN("[a-zA-Z0-9_-]+"),
+});
+
+export const Dot = createToken({
+  name: "Dot",
+  pattern: ".",
+});
+
+export const Eq = createToken({
+  name: "Eq",
+  pattern: "=",
+});
+
+/**
+ * Table
+ *
+ * table = std-table / array-table
+ */
+
+/**
+ * Standard Table
+ *
+ * std-table = std-table-open key std-table-close
+ *
+ * std-table-open  = %x5B ws     ; [ Left square bracket
+ * std-table-close = ws %x5D     ; ] Right square bracket
+ */
+
+/**
+ * Inline Table
+ *
+ * inline-table = inline-table-open [ inline-table-keyvals ] inline-table-close
+ *
+ * inline-table-open  = %x7B ws     ; {
+ * inline-table-close = ws %x7D     ; }
+ * inline-table-sep   = ws %x2C ws  ; , Comma
+ *
+ * inline-table-keyvals = key keyval-sep val [ inline-table-sep inline-table-keyvals ]
+ */
+
+export const LCurly = createToken({
+  name: "LCurly",
+  pattern: "{",
+});
+
+export const RCurly = createToken({
+  name: "RCurly",
+  pattern: "}",
+});
+
+/**
+ * Array Table
+ *
+ * array-table = array-table-open key array-table-close
+ *
+ * array-table-open  = %x5B.5B ws  ; [[ Double left square bracket
+ * array-table-close = ws %x5D.5D  ; ]] Double right square bracket
+ */
+
+export const DoubleLSquare = createToken({
+  name: "DoubleLSquare",
+  pattern: "[[",
+});
+
+export const DoubleRSquare = createToken({
+  name: "DoubleRSquare",
+  pattern: "]]",
+});
+
 export const TOKENS = [
+  LSquare,
+  RSquare,
+  Comma,
+  Dot,
+  Eq,
+  LCurly,
+  RCurly,
+  DoubleLSquare,
+  DoubleRSquare,
+
+  // Boolean
+  True,
+  False,
+
+  // Date and Time
+  OffsetDateTime,
+  LocalDateTime,
+  LocalDate,
+  LocalTime,
+
+  // Key
+  UnquotedKey,
+
+  // String
   Sq,
   ThreeSq,
   Dq,
@@ -183,6 +504,17 @@ export const TOKENS = [
   MultilineLiteralChar,
   BasicChar,
   MultilineBasicChar,
+
+  // Integer
+  DecimalInteger,
+  HexInteger,
+  OctInteger,
+  BinInteger,
+
+  // Float
+  Float,
+  SpecialFloat,
+
   Comment,
   Whitespace,
   Newline,
--- packages/toml/src/parser.ts
+++ packages/toml/src/parser.ts
@@ -1,27 +1,70 @@
 import { CstNode, CstParser } from "chevrotain";
 import {
   BasicChar,
+  BinInteger,
+  DecimalInteger,
   Dq,
+  HexInteger,
   LiteralChar,
   MultilineBasicChar,
   MultilineLiteralChar,
   Newline,
+  OctInteger,
   Sq,
   ThreeDq,
   ThreeSq,
   TOKENS,
+  Float,
+  SpecialFloat,
+  True,
+  False,
+  OffsetDateTime,
+  LocalDateTime,
+  LocalDate,
+  LocalTime,
+  Comma,
+  LSquare,
+  RSquare,
+  UnquotedKey,
+  Dot,
+  Eq,
+  LCurly,
+  RCurly,
+  DoubleLSquare,
+  DoubleRSquare,
 } from "./lexer";
 
 export class TOMLParser extends CstParser {
   public toml!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
   private expression!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private string!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
   private basicString!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
   private multilineBasicString!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
   private literalString!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
   private multilineLiteralString!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private boolean!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private datetime!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private float!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private integer!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private value!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private arrayValues!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private array!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private simpleKey!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private dottedKey!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private key!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private inlineTableKeyValues!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private stdTable!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private arrayTable!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
+  private inlineTable!: (idxInCallingRule?: number | undefined, ...args: any[]) => CstNode;
 
   constructor() {
-    super(TOKENS);
+    super(TOKENS, {
+      ignoredIssues: {
+        key: {
+          OR: true,
+        },
+      },
+    });
 
     const $ = this;
 
@@ -57,6 +100,52 @@ export class TOMLParser extends CstParser {
       ]);
     });
 
+    // val = string / boolean / array / inline-table / date-time / float / integer
+    $.RULE("value", () => {
+      $.OR([
+        {
+          ALT: () => {
+            $.SUBRULE($.string);
+          },
+        },
+        {
+          ALT: () => {
+            $.SUBRULE($.datetime);
+          },
+        },
+        {
+          ALT: () => {
+            $.SUBRULE($.boolean);
+          },
+        },
+        {
+          ALT: () => {
+            $.SUBRULE($.float);
+          },
+        },
+        {
+          ALT: () => {
+            $.SUBRULE($.integer);
+          },
+        },
+        {
+          ALT: () => {
+            $.SUBRULE($.array);
+          },
+        },
+        {
+          ALT: () => {
+            $.SUBRULE($.inlineTable);
+          },
+        },
+      ]);
+    });
+
+    /**
+     * String
+     *
+     * string = ml-basic-string / basic-string / ml-literal-string / literal-string
+     */
     $.RULE("string", () => {
       $.OR([
         {
@@ -82,6 +171,7 @@ export class TOMLParser extends CstParser {
       ]);
     });
 
+    // basic-string = quotation-mark *basic-char quotation-mark
     $.RULE("basicString", () => {
       $.CONSUME(Dq);
       $.MANY(() => {
@@ -90,6 +180,7 @@ export class TOMLParser extends CstParser {
       $.CONSUME1(Dq);
     });
 
+    // ml-basic-string = ml-basic-string-delim ml-basic-body ml-basic-string-delim
     $.RULE("multilineBasicString", () => {
       $.CONSUME(ThreeDq);
       $.MANY(() => {
@@ -109,6 +200,7 @@ export class TOMLParser extends CstParser {
       $.CONSUME1(ThreeDq);
     });
 
+    // literal-string = apostrophe *literal-char apostrophe
     $.RULE("literalString", () => {
       $.CONSUME(Sq);
       $.MANY(() => {
@@ -117,6 +209,7 @@ export class TOMLParser extends CstParser {
       $.CONSUME1(Sq);
     });
 
+    // ml-literal-string = ml-literal-string-delim ml-literal-body ml-literal-string-delim
     $.RULE("multilineLiteralString", () => {
       $.CONSUME(ThreeSq);
       $.MANY(() => {
@@ -134,6 +227,217 @@ export class TOMLParser extends CstParser {
         ]);
       });
       $.CONSUME1(ThreeSq);
+    });
+
+    /**
+     * Integer
+     *
+     * integer = dec-int / hex-int / oct-int / bin-int
+     */
+    $.RULE("integer", () => {
+      $.OR([
+        {
+          ALT: () => {
+            $.CONSUME(DecimalInteger);
+          },
+        },
+        {
+          ALT: () => {
+            $.CONSUME(HexInteger);
+          },
+        },
+        {
+          ALT: () => {
+            $.CONSUME(OctInteger);
+          },
+        },
+        {
+          ALT: () => {
+            $.CONSUME(BinInteger);
+          },
+        },
+      ]);
+    });
+
+    /**
+     * Float
+     *
+     * float = float-int-part ( exp / frac [ exp ] )
+     * float =/ special-float
+     */
+    $.RULE("float", () => {
+      $.OR([
+        {
+          ALT: () => {
+            $.CONSUME(Float);
+          },
+        },
+        {
+          ALT: () => {
+            $.CONSUME(SpecialFloat);
+          },
+        },
+      ]);
+    });
+
+    /**
+     * Boolean
+     *
+     * boolean = true / false
+     */
+    $.RULE("boolean", () => {
+      $.OR([
+        {
+          ALT: () => {
+            $.CONSUME(True);
+          },
+        },
+        {
+          ALT: () => {
+            $.CONSUME(False);
+          },
+        },
+      ]);
+    });
+
+    /**
+     * Date and Time (as defined in RFC 3339)
+     *
+     * date-time      = offset-date-time / local-date-time / local-date / local-time
+     */
+    $.RULE("datetime", () => {
+      $.OR([
+        {
+          ALT: () => {
+            $.CONSUME(OffsetDateTime);
+          },
+        },
+        {
+          ALT: () => {
+            $.CONSUME(LocalDateTime);
+          },
+        },
+        {
+          ALT: () => {
+            $.CONSUME(LocalDate);
+          },
+        },
+        {
+          ALT: () => {
+            $.CONSUME(LocalTime);
+          },
+        },
+      ]);
+    });
+
+    $.RULE("arrayValues", () => {
+      $.SUBRULE($.value);
+      $.OPTION(() => {
+        $.CONSUME(Comma);
+        $.SUBRULE($.arrayValues);
+      });
+    });
+
+    $.RULE("array", () => {
+      $.CONSUME(LSquare);
+      $.OPTION(() => {
+        $.SUBRULE($.arrayValues);
+      });
+      $.CONSUME(RSquare);
+    });
+
+    $.RULE("simpleKey", () => {
+      $.OR2([
+        {
+          ALT: () => {
+            $.SUBRULE($.literalString);
+          },
+        },
+        {
+          ALT: () => {
+            $.SUBRULE($.basicString);
+          },
+        },
+        {
+          ALT: () => {
+            $.CONSUME(UnquotedKey);
+          },
+        },
+      ]);
+    });
+
+    $.RULE("dottedKey", () => {
+      $.SUBRULE1($.simpleKey);
+      $.MANY(() => {
+        $.CONSUME(Dot);
+        $.SUBRULE2($.simpleKey);
+      });
+    });
+
+    $.RULE("key", () => {
+      $.OR([
+        {
+          ALT: () => {
+            $.SUBRULE1($.dottedKey);
+          },
+        },
+        {
+          ALT: () => {
+            $.SUBRULE1($.simpleKey);
+          },
+        },
+      ]);
+    });
+
+    $.RULE("keyValue", () => {
+      $.SUBRULE($.key);
+      $.CONSUME(Eq);
+      $.SUBRULE($.value);
+    });
+
+    $.RULE("stdTable", () => {
+      $.CONSUME(LSquare);
+      $.SUBRULE($.key);
+      $.CONSUME(RSquare);
+    });
+
+    $.RULE("inlineTableKeyValues", () => {
+      $.SUBRULE($.key);
+      $.CONSUME(Eq);
+      $.SUBRULE($.value);
+      $.MANY(() => {
+        $.CONSUME(Comma);
+        $.SUBRULE($.inlineTableKeyValues);
+      });
+    });
+
+    $.RULE("inlineTable", () => {
+      $.CONSUME(LCurly);
+      $.OPTION(() => {
+        $.SUBRULE($.inlineTableKeyValues);
+      });
+      $.CONSUME(RCurly);
+    });
+
+    $.RULE("arrayTable", () => {
+      $.CONSUME(DoubleLSquare);
+      $.SUBRULE($.key);
+      $.CONSUME(DoubleRSquare);
+    });
+
+    $.RULE("table", () => {
+      $.OR([
+        {
+          ALT: () => {
+            $.SUBRULE($.stdTable);
+          },
+        },
+        {
+          ALT: () => {
+            $.SUBRULE($.arrayTable);
+          },
+        },
+      ]);
     });
 
     this.performSelfAnalysis();
